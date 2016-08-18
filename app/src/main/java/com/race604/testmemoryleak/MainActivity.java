b3914a28bc3c1f49fcb3bdf254e48cdaa2a5785d
race604 <race604@gmail.com>
2016-08-18 21:55:38 +0800

    Init test memory leak app

---
+++ app/src/main/java/com/race604/testmemoryleak/MainActivity.java
@@ -0,0 +1,28 @@
+package com.race604.testmemoryleak;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new PrefsFragment())
+                    .commit();
+        }
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.test_preference);
+        }
+    }
+
+
+}
